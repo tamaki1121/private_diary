2b84f6b7afdc32c618dc96db3b4b34225baf75f4
tamaki1121 <koizumi88ohara@gmail.com>
2021-09-06 12:27:23 +0900

    form py create

--- diary/urls.py
+++ diary/urls.py
@@ -5,5 +5,6 @@ app_name = 'diary'
 urlpatterns = [
     path('', views.IndexView.as_view(), name="index"),
     path('base', views.BaseView.as_view(), name="base"),
+    path('inquiry/', views.InquiryView.as_view(), name="inquiry")
     # path('', views.index , name="index"),
 ]
--- diary/views.py
+++ diary/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
+from .forms import InquiryForm
 
 # Create your views here.
 # def index(request):
@@ -7,5 +8,10 @@ from django.views import generic
 
 class IndexView(generic.TemplateView):
     template_name = "diary/index.html"
+
 class BaseView(generic.TemplateView):
     template_name = "diary/base.html"
+
+class InquiryView(generic.FormView):
+    template_name = "diary/inquiry.html"
+    form_class = InquiryForm
