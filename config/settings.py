bea4b2caa3635a2602f8599ad3a69c42907891c2
tamaki1121 <koizumi88ohara@gmail.com>
2021-09-08 11:41:05 +0900

    create accounts そして初期化

--- config/settings.py
+++ config/settings.py
@@ -40,7 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # my apps
     'diary.apps.DiaryConfig',
+    'diary.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
