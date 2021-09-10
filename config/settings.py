cf5c3ad84480ea70e6f15de02ebdab88cfb8a25c
tamaki1121 <koizumi88ohara@gmail.com>
2021-09-10 12:08:51 +0900

    delete unused file edit account tempfile

--- config/settings.py
+++ config/settings.py
@@ -189,13 +189,22 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 
 # 
 SITE_ID = 1
-AUTHRNTICATION_BACKENDS = (
+AUTHENTICATION_BACKENDS = (
     'allauth.account.auth_backends.AuthenticationBackend',
     'django.contrib.auth.backends.ModelBackend',
 )
 
-ACCOUNT_AUTHRNTICATHION_METHD = 'email'
+ACCOUNT_AUTHENTICATION_METHOD = 'email'
 ACCOUNT_USERNAME_REQUIRED = False
 #
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 ACCOUNT_EMAIL_REQUIRED = True
+
+LOGIN_REDIRECT_URL = 'diary:index'
+ACCOUNT_LOGOUT_REDIRECT_URL = 'account_login'
+
+ACCOUNT_LOGOUT_ON_GET = True
+
+ACCOUNT_EMAIL_SUBJECT_PREFIX = ''
+
+DEFAULT_FROM_EMAIL = 'admin@example.com'
